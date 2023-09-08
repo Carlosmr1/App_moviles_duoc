e7b858d1957611fca7445751ecf947abf19cddc5
KimDonoSo <ki.donoso@duocuc.cl>
2023-09-07 20:14:49 -0400

    cambiosinicio

--- src/app/recuperar-con/recuperar-con.page.ts
+++ src/app/recuperar-con/recuperar-con.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-recuperar-con',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecuperarConPage implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
+   
+  }
+  cont(){
+    this.router.navigate(['/inicioses'])
   }
 
 }
